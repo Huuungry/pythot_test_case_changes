c9791792f6634aea407790990787e6d1016eecdc
Val9My <ValeriiMykhalchuk@gmail.com>
2018-07-04 15:29:42 +0300

    t_c_data added

--- test_case_changes/test_case_changes/JsonRequest.py
+++ test_case_changes/test_case_changes/JsonRequest.py
@@ -6,12 +6,12 @@ import bs4
 from xml.dom import minidom
 import xml.etree.ElementTree as ET
 
-import credentials
+import test_case_changes.credentials
 
 from test_case_changes.test_case_changes import credentials
 
-LOGIN = credentials.get_login()
-PASSWORD = credentials.get_password()
+LOGIN = test_case_changes.credentials.get_login()
+PASSWORD = test_case_changes.credentials.get_password()
 
 json_response = "test"
 test_case_id = "446114"
@@ -27,10 +27,28 @@ def get_json_response(test_case_id, test_case_rev):
     json_response = r.content.decode('utf-8')
     return json_response
 
+def get_t_c_data(test_case_id, test_case_rev):
+    parsed_t_c_id = json.loads(get_json_response(test_case_id, test_case_rev))
+    t_c_id = parsed_t_c_id['id']
+    t_c_rev = parsed_t_c_id['rev']
+    t_c_name = parsed_t_c_id['fields']['System.Title']
+    t_c_state = parsed_t_c_id['fields']['System.State']
+    return t_c_id, t_c_rev, t_c_name, t_c_state
+
+def get_t_c_id(test_case_id, test_case_rev):
+    return get_t_c_data(test_case_id, test_case_rev)[0]
+def get_t_c_rev(test_case_id, test_case_rev):
+    return get_t_c_data(test_case_id, test_case_rev)[1]
+def get_t_c_name(test_case_id, test_case_rev):
+    return get_t_c_data(test_case_id, test_case_rev)[2]
+def get_t_c_state(test_case_id, test_case_rev):
+    return get_t_c_data(test_case_id, test_case_rev)[3]
+
 def parse_json(test_case_id, test_case_rev):
     parsed_lib = json.loads(get_json_response(test_case_id, test_case_rev))
     steps_xml = parsed_lib['fields']['Microsoft.VSTS.TCM.Steps']
     return steps_xml
+
 
 def parse_xml(test_case_id, test_case_rev):
     document = minidom.parseString(parse_json(test_case_id, test_case_rev))
@@ -48,15 +66,26 @@ def parse_xml(test_case_id, test_case_rev):
 
 def parse_html(test_case_id, test_case_rev):
     soup = bs4.BeautifulSoup(parse_xml(test_case_id, test_case_rev), "html.parser")
-    return soup.text
+    #results = {} TOOODOOOOO
+
+    return soup.get_text(separator=" ")
 
 def difference(test_case_id, test_case_rev):
     old = parse_html(test_case_id, str((int(test_case_rev) - 1))).splitlines()
     new = parse_html(test_case_id, test_case_rev).splitlines()
-    diff_html = difflib.HtmlDiff().make_file(new, old)
-    return diff_html
+
+    #print (old, new)
+    diff_html = difflib.HtmlDiff().make_file(old, new, fromdesc='T-C %sRevision %s'%(test_case_id, str((int(test_case_rev) - 1))), todesc='T-C %sRevision %s'%(test_case_id, test_case_rev))
+    diff_html = diff_html.replace('&nbsp; ','&nbsp;')
+    diff_html = diff_html.replace('  &nbsp;', '&nbsp;')
+    return diff_html.replace(' &nbsp;', '&nbsp;')
 
 # print(parse_json(get_json_response(get_json_URL(test_case_id,test_case_rev))))
-# difference(test_case_id, test_case_rev)
-# print(parse_xml(test_case_id, test_case_rev))
-print( difference(test_case_id, test_case_rev))
+
+#print(parse_xml(test_case_id, test_case_rev))
+# print( difference(test_case_id, test_case_rev))
+#difference(test_case_id, test_case_rev)
+print(get_t_c_id(test_case_id, test_case_rev))
+print(get_t_c_rev(test_case_id, test_case_rev))
+print(get_t_c_name(test_case_id, test_case_rev))
+print(get_t_c_state(test_case_id, test_case_rev))
